5f7b9f17871088aba062386fed5b2b3a5c3f994d
cormac <cormac@studiorua.ie>
2013-06-12 15:34:51 +0100

    added location for test db

--- corroborator/settings/test.py
+++ corroborator/settings/test.py
@@ -1,5 +1,16 @@
 from settings.common import *
 
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3', 
+        'NAME': '/var/lib/jenkins/db/corroborator.sql', # Or path to database file if using sqlite3.
+        'USER': '', # Not used with sqlite3.
+        'PASSWORD': '',# Not used with sqlite3.
+        'HOST': '',# Set to empty string for localhost. Not used with sqlite3.
+        'PORT': '',# Set to empty string for default. Not used with sqlite3.
+    }
+}
+
 INSTALLED_APPS += (
     'autofixture',
     'interntest',
