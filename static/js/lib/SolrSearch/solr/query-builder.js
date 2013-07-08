7807e726da83091f972a5c0d35033044786f8bc9
cormac <cormac@studiorua.ie>
2013-07-08 15:09:01 +0100

    added query builder file and test file

---
+++ static/js/lib/SolrSearch/solr/query-builder.js
@@ -0,0 +1,20 @@
+/*global define */
+// Author: Cormac McGuire
+// ### Description
+// build the solr query based on filters sent
+
+define (
+  [
+    'lib/streams'
+  ],
+  function (Streams) {
+    'use strict';
+
+
+    var init = function () {
+    };
+    return {
+      init: init
+    };
+});
+
