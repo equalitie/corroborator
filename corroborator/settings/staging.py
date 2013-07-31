73d31708c9c030b979dede3c083de24043d13d29
cromac <cormac2010@gmail.com>
2013-07-31 14:54:35 +0000

    added staging config and fixture data

---
+++ corroborator/settings/staging.py
@@ -0,0 +1,47 @@
+from settings.common import *
+
+SOLR_CORE = 'corrob_staging'
+
+DATABASES = {
+    'default': {
+    'ENGINE': 'django.db.backends.mysql', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
+    'NAME': 'corroborator_staging',                      # Or path to database file if using sqlite3.
+    'USER': 'django',                      # Not used with sqlite3.
+    'PASSWORD': 'pe2ua8Yahp7fainoop7U',                  # Not used with sqlite3.
+    'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
+    'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
+                                                                }
+}
+
+
+#Haystack backend configuration
+HAYSTACK_SIGNAL_PROCESSOR = 'haystack.signals.RealtimeSignalProcessor'
+
+HAYSTACK_CONNECTIONS = {
+    'default': {
+        'ENGINE': 'haystack.backends.solr_backend.SolrEngine',
+        'URL': 'https://sjac.rightscase.org/solr'
+        # ...or for multicore...
+    },
+}
+
+# Absolute path to the directory static files should be collected to.
+# Don't put anything in this directory yourself; store your static files
+# in apps' "static/" subdirectories and in STATICFILES_DIRS.
+# Example: "/home/media/media.lawrence.com/static/"
+STATIC_ROOT = '/opt/corroborator/dev/static/'
+
+# URL prefix for static files.
+# Example: "http://media.lawrence.com/static/"
+STATIC_URL = '/static/'
+
+# Additional locations of static files
+STATICFILES_DIRS = (
+    ('stylesheets', 'static/stylesheets'),
+    ('js', 'static/js'),
+    ('images', 'static/images'),
+    ('fonts', 'static/fonts'),
+    # Put strings here, like "/home/html/static" or "C:/www/django/static".
+    # Always use forward slashes, even on Windows.
+    # Don't forget to use absolute paths, not relative paths.
+)
