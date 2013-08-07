0943c6cb83ca9549d7e7461161fa142516094fa7
cormac <cormac@studiorua.ie>
2013-08-07 12:31:34 +0100

    updated IncidentApi and search_indexes to reflect model element name change

--- corroborator_app/search_indexes.py
+++ corroborator_app/search_indexes.py
@@ -206,7 +206,7 @@ class IncidentIndex(indexes.SearchIndex, indexes.Indexable):
         Returns the correctly formated uri related to this incident instance
         for the tastypie api
         """
-        return ['/api/v1/bulletin/{0}/'.format(bulletin.id) for bulletin in object.bulletins.all()]
+        return ['/api/v1/bulletin/{0}/'.format(bulletin.id) for bulletin in object.ref_bulletins.all()]
     def prepare_actors_role(self, object):
         """
         Returns the correctly formated uri related to this incident instance
@@ -263,7 +263,7 @@ class IncidentIndex(indexes.SearchIndex, indexes.Indexable):
         """
         Returns count of Bulletin objects associated with a given Incident
         """
-        return object.bulletins.count()
+        return object.ref_bulletins.count()
 
     def prepare_incident_locations(self, object):
         """
