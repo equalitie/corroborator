69e43ddb353181fdb3064ed3ea2a0b3b4f6e9d86
bill <bill@ns4004403.ip-142-4-214.net>
2013-08-15 13:31:37 +0000

    adding socketio

--- corroborator_app/tasks.py
+++ corroborator_app/tasks.py
@@ -1,9 +1,13 @@
 from celery import task
 from haystack.management.commands import update_index
+from django_socketio import broadcast_channel
 
 @task
 def update_object():
     update_index.Command().handle()
+    updated = {"action": "updated"}
+    channel = 'corroborator_solr_update'
+    broadcast_channel(updated, channel)
 
 @task
 def remove_object(index, instance, using=None):
