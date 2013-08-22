6a6d79283dc13c81fd37d93e0c9e1c2b4e63c135
cromac <cormac2010@gmail.com>
2013-08-22 23:36:23 +0000

    fixed staging file

--- corroborator/settings/staging.py
+++ corroborator/settings/staging.py
@@ -15,8 +15,8 @@ DATABASES = {
 
 
 #Haystack backend configuration
-HAYSTACK_SIGNAL_PROCESSOR = 'haystack.signals.RealtimeSignalProcessor'
-#HAYSTACK_SIGNAL_PROCESSOR = 'celery_haystack.signals.CelerySignalProcessor'
+#HAYSTACK_SIGNAL_PROCESSOR = 'haystack.signals.RealtimeSignalProcessor'
+HAYSTACK_SIGNAL_PROCESSOR = 'celery_haystack.signals.CelerySignalProcessor'
 
 
 HAYSTACK_CONNECTIONS = {
