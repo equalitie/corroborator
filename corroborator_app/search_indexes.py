d368bd25887707fb0bc92c18e4635c9593e463f1
bill <bill@ns4004403.ip-142-4-214.net>
2013-08-23 16:12:36 +0000

    added checking code for media file existence

--- corroborator_app/search_indexes.py
+++ corroborator_app/search_indexes.py
@@ -155,13 +155,13 @@ class MediaIndex(CelerySearchIndex, indexes.Indexable):
         """
         #return object.get_uri()
 
-        if object.media_file != None:
+        if object.media_file.name != '':
             return common.S3_URL + '/' + object.media_file.name
         else:
             ''
     def prepare_media_thumb_file(self, object):
         #return object.get_thumb_uri()
-        if object.media_thumb_file != None:
+        if object.media_thumb_file.name != '':
             return common.S3_URL + '/' + object.media_thumb_file.name
         else:
             ''
