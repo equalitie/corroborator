2a9b4b4ef58df2c3a518d76870182e01aa5d6bbe
cormac <cormac@studiorua.ie>
2013-09-25 12:43:02 +0100

    edited settings locations

--- corroborator_app/index_meta_prep/mediaURLGetter.py
+++ corroborator_app/index_meta_prep/mediaURLGetter.py
@@ -1,7 +1,8 @@
-from corroborator.settings import common
+from django.conf import settings
 from django.core.cache import cache
 from corroborator_app.tasks import fetch_from_s3
 import os
+
 
 def getMediaUrl(media_file):
     """
@@ -13,15 +14,15 @@ def getMediaUrl(media_file):
 
         filename = media_file.name.split("/")[-1]
             
-        filepath = os.path.join(common.CACHE_PATH, filename)
+        filepath = os.path.join(settings.CACHE_PATH, filename)
         if not os.path.exists(filepath):
             
             outfile = open(filepath, "w")
             outfile.write(cacheget)
             outfile.close()
 
-        return common.CACHE_URL + filename
+        return settings.CACHE_URL + filename
     else:
-        s3_url = common.S3_URL + '/' + media_file.name
+        s3_url = settings.S3_URL + '/' + media_file.name
         fetch_from_s3.delay(media_file.name, s3_url)
         return s3_url
