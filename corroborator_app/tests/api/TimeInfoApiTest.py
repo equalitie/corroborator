081bc7949c690e305c40e4b4b6922cc6522acbf7
cormac <cormac@studiorua.ie>
2013-10-25 14:00:47 +0100

    updated timeinfo test time generation

--- corroborator_app/tests/api/TimeInfoApiTest.py
+++ corroborator_app/tests/api/TimeInfoApiTest.py
@@ -1,11 +1,11 @@
 from tastypie.models import ApiKey
 from django.contrib.auth.models import User
-from django.test.client import Client
 from tastypie.test import ResourceTestCase
 from autofixture import AutoFixture
 from corroborator_app.models import TimeInfo
 import datetime
-from django.utils.timezone import utc
+from django.utils import timezone
+
 
 class TimeInfoTestCase(ResourceTestCase):
     def setUp(self):
@@ -13,10 +13,10 @@ class TimeInfoTestCase(ResourceTestCase):
         self.user = User(username='user', password='password', email='1@2.com')
         self.user.save()
         fixture = AutoFixture(TimeInfo)
-        timeInfos = fixture.create(10)
-        now = datetime.datetime.utcnow().replace(tzinfo=utc)
-        self.from_datetime = now.strftime('%Y-%m-%dT%H:%M:%S.%fZ')
-        self.to_datetime = now.strftime('%Y-%m-%dT%H:%M:%S.%fZ')
+        fixture.create(10)
+        now = timezone.now()
+        self.from_datetime = now.isoformat()
+        self.to_datetime = now.isoformat()
         try:
             self.api_key = ApiKey.objects.get(user=self.user)
         except ApiKey.DoesNotExist:
@@ -48,7 +48,8 @@ class TimeInfoTestCase(ResourceTestCase):
 
     def test_timeInfo_put(self):
         ti = TimeInfo.objects.all()[0]
-        url = '/api/v1/timeInfo/{0}/?format=json{1}'.format(ti.id, self.auth_string)
+        url = '/api/v1/timeInfo/{0}/?format=json{1}'.format(
+            ti.id, self.auth_string)
         put_data = {
             'time_from': self.from_datetime,
             'time_to': self.to_datetime,
@@ -57,7 +58,7 @@ class TimeInfoTestCase(ResourceTestCase):
         }
         response = self.api_client.put(url, data=put_data)
         self.assertEqual(response.status_code, 202)
-        
+
     def test_timeInfo_patch(self):
         url = '/api/v1/timeInfo/?format=json{}'.format(self.auth_string)
         patch_data = {
@@ -78,4 +79,3 @@ class TimeInfoTestCase(ResourceTestCase):
         }
         response = self.api_client.patch(url, data=patch_data)
         self.assertEqual(response.status_code, 202)
-        
