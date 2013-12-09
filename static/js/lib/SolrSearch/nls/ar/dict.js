d518f72031e978dc449234d75e0f4ef5ea371a20
cormac <cormac@studiorua.ie>
2013-12-09 17:30:41 +0000

    saving config to backend

---
+++ static/js/lib/SolrSearch/nls/ar/dict.js
@@ -0,0 +1,86 @@
+// standard strings for header.js view
+define({
+    'sort': {
+      'date': 'تاريخ',
+      'location': 'مكان',
+      'title': 'عنوان',
+      'status': 'حالة',
+      'score': 'درجة',
+      'confirm_delete_header': 'حذف العناصر المحددة؟',
+      'confirm_delete_message': 'سيتم حذف هذه العناصر. هل أنت متأكد؟',
+      'confirm': 'تأكيد',
+      'cancel': 'إلغاء الأمر'
+    },
+    'count': {
+      'selected': 'تحديد'
+    },
+    'filters': {
+      'new_actor': 'شخص جديد',
+      'new_bulletin': 'مادة جديدة',
+      'new_incident': 'ملف جديد',
+      'and': 'و',
+      'current_filters': 'الفلاتر المفعلة',
+      'Enter_text_here_to_see_available_filters': 'ابدأ بالطباعة لتظهرالفلاتر المتوفرة',
+      'num_results': 'نتيجة <%=numItems %>.',
+      'bulletin_labels_exact':'تصنيفات المادة',
+      'bulletin_assigned_user_exact':'المستخدم المسؤول',
+      'bulletin_sources_exact':'مصادر',
+      'bulletin_created_exact':'تاريخ الإنشاء',
+      'most_recent_status_bulletin_exact':'حالة',
+      //Incident fields
+      'confidence_score_exact':'الموثوقية',
+      'incident_times_exact':'تاريخ\\الساعة الملف',
+      'incident_labels_exact':'تصنيف الملف', 
+      'incident_assigned_user_exact':'المستخدم المسؤول',
+      'incident_crimes_exact':'أنواع الملفات',
+      'incident_created_exact':'تاريخ الإنشاء',
+      'most_recent_status_incident_exact':'حالة',
+      //Actor fields
+      'age_en_exact':'سن (EN)',
+      'age_ar_exact':'سن (ع)',
+      'sex_en_exact':'جنس (EN)',
+      'sex_ar_exact':'جنس (ع)',
+      'civilian_en_exact':' (ع)',
+      'civilian_ar_exact':'Civilian',
+      'nationality_en_exact':'الجنسية (EN)',
+      'nationality_ar_exact':'الجنسية (ع)',
+      'occupation_en_exact':'احتلال (EN)',
+      'occupation_ar_exact':'احتلال (ع)',
+      'position_en_exact':'موقف (EN)',
+      'position_ar_exact':'موقف (ع)',
+      'ethnicity_en_exact':'العرق (EN)',
+      'ethnicity_ar_exact':'العرق (ع)',
+      'spoken_dialect_en':'لهجة(en)',
+      'spoken_dialect_ar':'لهجة(ع)',
+      'incident_searchable_locations_exact':'الحادث الموقع',
+      'bulletin_searchable_locations_exact':'نشرة الموقع',
+      'actor_searchable_pob_exact':'مكان الميلاد',
+      'actor_searchable_current_exact':'الموقع الحالي'
+    },
+    'results': {
+      'aka': 'الملقب',
+      'lives_in': 'يعيش في <placename>',
+      'works_as_a': 'يعمل <jobname>',
+      'involved_in': 'involved in',
+      'involved': 'متورط',
+      'no_results': 'لا نتائج',
+      'in': 'في'
+    },
+    'menu': {
+      'Delete_selected': 'حذف مختارة',
+      'Update_selected': 'تحديث مختارة',
+      'Select_all': 'تحديد كافة',
+      'Clear_selected': 'مسح المحدد',
+    },
+    'dialog': {
+      'Save_search_as': 'حفظ البحث ك',
+      'Save_search': 'حفظ البحث',
+      'Cancel': 'إلغاء',
+      'Search_title': 'عنوان البحث',
+      'Share_search_with_other_users': 'حصة البحث مع مستخدمين آخرين',
+      'Search_saved': 'البحث المحفوظة',
+      'Saving_search': 'إنقاذ بحث',
+      'Error_saving_search': 'الخطأ البحث الصيفي',
+      'Length_error': 'يجب أن يكون طول بحث محفوظ بين <%=min %> وشخصيات <%=max %>'
+    }
+});
