4f3f89a429f4d60b13c0493cc336dba151287492
cormac <cormac@studiorua.ie>
2014-01-20 18:41:23 +0000

    defensive coding

--- corroborator_app/search_indexes.py
+++ corroborator_app/search_indexes.py
@@ -338,7 +338,11 @@ class IncidentIndex(CelerySearchIndex, indexes.Indexable):
         """
         return date portion of incident created datetime field
         """
-        return object.incident_created.date()
+
+        if object.incident_created is not None:
+            return object.incident_created.date()
+        else:
+            return ''
 
     def prepare_incident_modified_date(self, object):
         """
@@ -543,7 +547,10 @@ class BulletinIndex(CelerySearchIndex, indexes.Indexable):
         """
         return date portion of bulletin created datetime field
         """
-        return object.bulletin_created.date()
+        if object.bulletin_created is not None:
+            return object.bulletin_created.date()
+        else:
+            return ''
 
     def prepare_bulletin_modified_date(self, object):
         """
